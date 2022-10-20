c5c6101db5caca4c5885fd7d3eb6618c3cecea5f
BAEKDODAM <56225917+BAEKDODAM@users.noreply.github.com>
2022-10-20 15:53:13 +0900

    제품 상세정보 텝UI, 애니메이션 추가, redux 셋팅

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Button, Navbar, Container, Nav, Row, Col, NavLink } from 'react-bootstr
 import data from './data.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Detail';
-
+import axios from 'axios';
+import Cart from './routes/Cart';
 
 function App() {
-  let [shoes] = useState(data); //서버에서 가져온 데이터라고 가정
+  let [shoes, setShoes] = useState(data); //서버에서 가져온 데이터라고 가정
   let navigate = useNavigate(); // 페이지 이동 도와줌
+  let n=0;
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -26,21 +28,32 @@ function App() {
         <Route path='/' element={
           <>
           <div className="main-bg"></div>
-          <Container>
-            <Row>
+          <div className="container">
+            <div className="row">
             {
-                shoes.map((a,i)=>{
-                  return (
-                    <Card shoes={shoes[i]} i={i}></Card>
-                  )
-                })
-              }
-            </Row>
-          </Container>
+              shoes.map((a,i)=>{
+                return (
+                  <Card shoes={shoes[i]} i={i}></Card>
+                )
+              })
+            }
+            </div>
+          </div>
+          <button onClick={()=>{
+            n+=1
+            if(n==1) {
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((결과)=>{ 
+                let copy = [...shoes, ...결과.data];
+                setShoes(copy);
+              }) // 요청 결과는 axios.get('url').then()
+              .catch(()=>{console.log('실패')})
+            }
+          }}>더보기</button>
           </>
         }/>
-
         <Route path='/detail/:id' element={ <Detail shoes={shoes} /> } />
+        <Route path="/cart" element={ <Cart/> }/>
         <Route path='*' element={<div>없는 페이지</div> } />
       </Routes>
     </div>
@@ -50,11 +63,12 @@ function App() {
 
 function Card(props) {
   return (
-    <Col>
-      <img src={"https://codingapple1.github.io/shop/shoes"+(props.i+1)+".jpg"} width="80%"></img>
-        <h4>{props.shoes.title}</h4>
-        <p>{props.shoes.content}</p>
-    </Col>
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg'} width="80%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
+    </div>
+    
   )
 }
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
-
-
+import {Nav} from 'react-bootstrap';
 function Detail(props) {
 
     let {id} = useParams();
     let 찾은상품 = props.shoes.find((x)=> x.id == id )
     let [alert, setAlert] = useState(true)
-
+    let [tab, setTab] = useState(0)
+    let [fade2, setFade2] = useState('')
     useEffect(()=>{// Detail 컴포넌트가 mount, update될 때 실행됨. html  랜더링 후 동작함.
-        setTimeout(()=>{ setAlert(false) },2000)
-    }, [])
+        let a = setTimeout(()=>{ setAlert(false)},2000)
+        setFade2('end')
+        return ()=>{
+            // 기존 타이머 제거, 타이머 사용할 때 안전하게 사용 가능
+            clearTimeout(a)
+            setFade2('')
+        } // useEffect 동작 전에 실행됨, 기존 코드 지우는 거 여기에 많이 작성함
+
+    }, []) // useEffect 실행조건 넣을 수 있는 곳은 [], [] 안이 변할 때마다 실행됨
     return(
-        <div className="container">
+        <div className="container" >
+            <div className={`container start ${fade2}`}>
             {
                 alert == true ? <div className="alert alert-warning">2초 이내 구매시 할인</div> : null
             }
+            </div>
             <div className="row">
                 <div className="col-md-6">
                 <img src={"https://codingapple1.github.io/shop/shoes"+(찾은상품.id+1)+".jpg"} width="100%" />
@@ -28,8 +37,40 @@ function Detail(props) {
                 <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link0" onClick={()=>setTab(0)}>버튼 0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link1" onClick={()=>setTab(1)}>버튼 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link2" onClick={()=>setTab(2)}>버튼 2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent tab={tab}/>
+
         </div> 
     )
 }
+
+function TabContent({tab}) {
+    let [fade, setFade] = useState('')
+
+    useEffect (()=>{
+        setTimeout(()=> { setFade('end')  }, 100)
+        
+        return ()=>{ // clean up function: useEffect 동작하기 전에 실행하고자 하는 코드 작성
+            setFade('')
+        }
+    }, [tab])
+
+    return (
+    <div className={`start ${fade}`}>
+        {[ <div>내용0</div>, <div>내용1</div>, <div>내용2</div> ][tab]}
+    </div>)
+} 
+
+
 
 export default Detail;
